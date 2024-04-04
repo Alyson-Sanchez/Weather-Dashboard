497dc69415fcc4f0e24dd2166e3ad2bec7f1cbf3
Alyson Sanchez <coqui0429@gmail.com>
2024-04-03 23:11:41 -0400

    Functioning 5 day forcast

--- 02-Challenge/index.js
+++ 02-Challenge/index.js
@@ -10,7 +10,11 @@ const sortURL = async(promise) => {
 
     document.getElementsByTagName('h2')[1].innerHTML = jsonApi.city.name;
     console.log(fiveDayForcast);
-    populateDay(jsonApi.list[8], fiveDayForcast[0]);
+
+    for (let i=0; i< fiveDayForcast.length; i++){
+    populateDay(jsonApi.list[((i + 1) * 7)], fiveDayForcast[i]);
+    console.log(populateDay);
+    }
 }
 
 //returns a promise with an object 
@@ -27,25 +31,43 @@ function getApi(){
         //var finalTest = testString.then(result => result.data);
         sortURL(testString);
     }
-//assigns the value of the method to apiData
+    //assigns the value of the method to apiData
     apiData = getApi();
     
     var temp = kelvinToFarenheit(289.78);
     console.log(temp);
 
     //===========================================================================================================
-    //helper functions:
+    //HELPER FUNCTIONS :
     function kelvinToFarenheit(Kelvin){
         return((Kelvin - 273.15) * 9 / 5 + 32)
     }
 
+    // This function is ONLY for the 5 day forcast. The current day uses a different function entirely.
     function populateDay(dayJSON, div){
         //calculating the date of the day thats passed in
+        
+        //calculates a new date from the dayJSON (which references the API). This date is given to us in seconds, so it needs to be converted to miliseconds(multiplied by 1000) in order for the following function to have a readable value
         var calculatedDate = new Date(dayJSON.dt * 1000);
-       div.children[0].innerHTML = (calculatedDate.getMonth() +1 )+ '/' + calculatedDate.getDay() + '/' + calculatedDate.getFullYear();
+        //Replaces the first child of the div container with the result of the calculated month, day, and year
+       div.children[0].innerHTML = (calculatedDate.getMonth() +1 )+ '/' + calculatedDate.getDate() + '/' + calculatedDate.getFullYear();
+    
+       var iconURL = "http://openweathermap.org/img/w/" + dayJSON.weather[0].icon+ ".png";
+       div.children[1].src = iconURL;
 
+       //takes the result of the temp < main < JSON - which is in Kelvin - and assigns it to dayTempKelvin
+       var dayTempKelvin = dayJSON.main.temp;
+       // runs the kelvinToFarenheit function established earlier with the value of dayTempKelvin passed into it. .toFixed forces the result to be limited to the number of decimal places given in the parameter
+       var dayTemp = kelvinToFarenheit(dayTempKelvin).toFixed(2);
+       div.children[2].innerHTML = 'Temp: ' + dayTemp + '°F';
 
+        var dayWind = 'Wind: ' + dayJSON.wind.speed + 'MPH';
+        div.children[3].innerHTML = dayWind;
+
+       var dayHumid = 'Humidity: ' + dayJSON.main.humidity + ' %';
+       div.children[4].innerHTML = dayHumid;
     }
+
 
 /*function fetch(url)
 {
